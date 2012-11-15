7b8a0de36000035028333009d6e6a39918eb8594
soarpenguin <soarpenguin@gmail.com>
2012-11-15 20:08:42 +0800

    uname.c - get system infomation in run time use c.

---
+++ uname.c
@@ -0,0 +1,33 @@
+/*
+ * code sample for get the system infomation
+ *	    such as: sysname, nodename, release, 
+ *				 version, machine.
+ */
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/utsname.h>
+int 
+main(int argc,char**argv)
+{
+	int z;
+	struct utsname u_name;
+	z = uname(&u_name);
+	if(z == -1)
+	{
+		fprintf(stderr,"%s:uname(2)\n",strerror(errno));
+		exit (1);
+	}
+
+	printf("System infomation: \n");
+	printf(" sysname[] = %s;\n", u_name.sysname);/*系统名*/
+	printf(" nodename[] = %s;\n", u_name.nodename);
+	printf(" release[] = %s;\n", u_name.release);
+	printf(" version[] = %s;\n", u_name.version);/*版本*/
+	printf(" machine[] = %s;\n", u_name.machine);
+	
+	return 0;
+} 
