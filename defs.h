d1774f2c1cd7c150c1d0cf58c3986d7b1cd889d0
soarpenguin <soarpenguin@gmail.com>
2014-04-03 23:14:11 +0800

    add defs.h

---
+++ defs.h
@@ -0,0 +1,20 @@
+#if !defined __GNUC__
+# define __attribute__(x) /*nothing*/
+#endif
+
+#ifndef offsetof
+# define offsetof(type, member) \
+     (((char *) &(((type *) NULL)->member)) - ((char *) (type *) NULL))
+#endif
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+/* macros */
+#ifndef MAX
+# define MAX(a, b)      (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef MIN
+# define MIN(a, b)      (((a) < (b)) ? (a) : (b))
+#endif
+#define CLAMP(val, min, max) MIN(MAX(min, val), max)
+
