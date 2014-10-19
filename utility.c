7961fc63bb2cc77a9fbd70ed376ee090f50ef41f
soarpenguin <soarpenguin@gmail.com>
2014-10-19 22:30:19 +0800

    utility.c - add some MICRO for log.

--- utility.c
+++ utility.c
@@ -2,7 +2,37 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "error.h"
+//#include "error.h"
+
+#include <signal.h>
+#include <syslog.h>
+//#include "config.h"
+
+extern int quiet_mode;
+extern int use_syslog;
+
+/* Macros for logging/displaying status messages */
+#define PRINT(x...) { if (!quiet_mode) { fprintf(stderr, x); fprintf(stderr, "\n"); } }
+#define WARN(x...) { fprintf(stderr, "Warning: " x); fprintf(stderr, "\n"); }
+#define LOG(x...) { if (use_syslog) { openlog(PROG_NAME, LOG_PID, LOG_DAEMON); syslog(LOG_ERR, x); closelog(); } }
+#define DIE(x...) { fprintf(stderr, "Error: " x); fprintf(stderr, "\n"); raise(SIGINT); }
+#define LOG_PRINT(x...) { LOG(x); PRINT(x); }
+#define LOG_WARN(x...) { LOG(x); WARN(x); }
+#define LOG_DIE(x...) { LOG(x); DIE(x); }
+
+/* Assert macro for testing and debugging; use 'make debug'
+   to compile the program with debugging features enabled */
+#ifdef DEBUG
+#define ASSERT(x)                                                    \
+        if (!(x)) {                                                  \
+                fflush(NULL);                                        \
+                fprintf(stderr, "\nAssertion failed: %s, line %d\n", \
+                                __FILE__, __LINE__);                 \
+                fflush(stderr);                                      \
+                exit(EXIT_FAILURE);                                  \
+        }
+#endif
+
 
 /* Strip leading and trailing spaces from parameter string, modifying
    the string in place and returning a pointer to the (potentially)
