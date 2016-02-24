1a8f36b4bf5883aaa9403e263586c9f2a06c174b
soarpenguin <soarpenguin@gmail.com>
2016-02-24 22:49:10 +0800

    add memory cgroup usage sample code.

---
+++ docker/mem_cgroup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+ 
+int main(void)
+{
+    int size = 0;
+    int chunk_size = 512;
+    void *p = NULL;
+ 
+    while(1) {
+ 
+        if ((p = malloc(chunk_size)) == NULL) {
+            printf("out of memory!!\n");
+            break;
+        }
+
+        memset(p, 1, chunk_size);
+        size += chunk_size;
+        printf("[%d] - memory is allocated [%8d] bytes \n", getpid(), size);
+        sleep(1);
+    }
+
+    return 0;
+}
+
+
+//# 创建memory cgroup
+//$ mkdir /sys/fs/cgroup/memory/fengye
+//$ echo 64k > /sys/fs/cgroup/memory/fengye/memory.limit_in_bytes
+// 
+//# 把上面的进程的pid加入这个cgroup
+//$ echo [pid] > /sys/fs/cgroup/memory/fengye/tasks
+
